0cfcf0f161723c4f6a4b4f26d949b4b94122ff43
lia <gukamelia707@gmail.com>
2025-06-30 04:08:13 +0300

    Add files via upload

---
+++ 35.2Гук/MainCourse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _35._2Гук
+{
+    class MainCourse : Dish
+    {
+        public int SpicinessLevel { get; set; } // Уровень остроты (1-10)
+
+        public MainCourse(string name, decimal price, int calories, int preparationTime, int spicinessLevel)
+            : base(name, price, calories, preparationTime)
+        {
+            SpicinessLevel = spicinessLevel;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Основное блюдо: {Name}, Цена: {Price:C}, Калории: {Calories}, Время приготовления: {PreparationTime} мин, Острота: {SpicinessLevel}/10");
+        }
+    }
+}
